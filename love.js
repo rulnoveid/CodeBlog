3c2935d2e04d8123f0f68ccb1fe9741b4ae72c5c
Muhamad Sahrudin <36272872+rulnoveid@users.noreply.github.com>
2021-08-15 20:14:36 +0700

    Add files via upload

---
+++ love.js
@@ -0,0 +1,8 @@
+      gapi.load("gapi.iframes:gapi.iframes.style.bubble", function() {
+        if (gapi.iframes && gapi.iframes.getContext) {
+          gapi.iframes.getContext().openChild({
+              where: document.getElementById("navbar-iframe-container"),
+              id: "navbar-iframe"
+          });
+        }
+      });
