a2725415e558cdf650b2482424d50b814f99566f
Muhamad Sahrudin <36272872+rulnoveid@users.noreply.github.com>
2022-07-17 21:31:47 +0700

    Update ipgeo2.js

--- ipgeo2.js
+++ ipgeo2.js
@@ -41,3 +41,41 @@ function gotData(data) {
 function removeDummy() {
 	document.getElementById('defaultCanvas0').parentNode.removeChild(document.getElementById('defaultCanvas0'));
 }
+
+
+function tableToCSV(table, filename) {
+  var data = [];
+  var rows = document.querySelectorAll(`${table} tr`);
+  for (var i = 0; i < rows.length; i++) {
+    var row = [],
+      cols = rows[i].querySelectorAll("td, th");
+    for (var j = 0; j < cols.length; j++) {
+      row.push(cols[j].innerText);
+    }
+    data.push(row.join(","));
+  }
+  downloadCSV(data.join("\n"), filename);
+}
+
+function downloadCSV(csv, filename) {
+  var csv_file, download_link;
+  csv_file = new Blob([csv], { type: "text/csv" });
+  download_link = document.createElement("a");
+  download_link.download = filename;
+  download_link.href = window.URL.createObjectURL(csv_file);
+  download_link.style.display = "none";
+  document.body.appendChild(download_link);
+  download_link.click();
+}
+
+function capitalizeAll(str) {
+  try {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    return String(words).replace(/[,]/g, " ");
+  } catch (err) {
+    console.log(err);
+  }
+}
