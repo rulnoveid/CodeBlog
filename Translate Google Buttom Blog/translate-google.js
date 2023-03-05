5d042b04e56ac8a50c28f2f5dbb5448cf3a32036
Muhamad Sahrudin <36272872+rulnoveid@users.noreply.github.com>
2023-03-05 16:19:59 +0700

    Add files via upload

---
+++ Translate Google Buttom Blog/translate-google.js
@@ -0,0 +1,10 @@
+<!--[ Translate Google ]-->
+<!--[ Script WendyCode ]-->
+<script>/*<![CDATA[*/
+var lazyts=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyts||0!=document.body.scrollTop&&!1===lazyts)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.jsdelivr.net/gh/choipanwendy/translategoogle@main/translate2.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyts=!0)},!0);
+/*]]>*/
+</script>
+    
+<script>
+function googleTranslateElementInit(){new google.translate.TranslateElement({pageLanguage:&#39;id&#39;,includedLanguages:&#39;id,en,ru,sq,ar,zh,ms,uk&#39;,layout:google.translate.TranslateElement.InlineLayout.SIMPLE},&#39;google_translate_element&#39;)}$(&quot;.hover&quot;).mouseleave(function (){$(this).removeClass(&quot;hover&quot;)});
+</script>
